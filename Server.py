d1584ed90f7fa0b03400d9494df30a98cb11b06f
ShenganShi <shengan1220@gmail.com>
2024-03-08 18:08:56 -0500

    database update

--- Server.py
+++ Server.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from pymongo import MongoClient
 app = Flask(__name__)
-
  
-mongo_client = MongoClient("mongo")
-db = mongo_client['user_atabase']
-collection = db['user_infor']
+
+try:
+
+    mongo_client = MongoClient("mongo")
+    db = mongo_client['user_atabase']
+    collection = db['user_infor']
+
+except Exception as erro:
+    print("error in connecting to the Database")
 
 
 
