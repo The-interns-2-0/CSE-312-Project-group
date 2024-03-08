2d8a328e889da8a35def5970469cf708b546e937
ShenganShi <shengan1220@gmail.com>
2024-03-08 18:03:47 -0500

    1

--- Server.py
+++ Server.py
@@ -1,6 +1,14 @@
 from flask import Flask
-
+from pymongo import MongoClient
 app = Flask(__name__)
+
+
+mongo_client = MongoClient("mongo")
+db = mongo_client['user_atabase']
+collection = db['user_infor']
+
+
+
 
 @app.route("/")
 def hello_world():
