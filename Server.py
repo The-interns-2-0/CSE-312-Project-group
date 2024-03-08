1758e12b5fa52431546b10391d76ad9138ef0dd2
ShenganShi <shengan1220@gmail.com>
2024-03-08 18:04:50 -0500

    2

--- Server.py
+++ Server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from pymongo import MongoClient
 app = Flask(__name__)
 
-
+ 
 mongo_client = MongoClient("mongo")
 db = mongo_client['user_atabase']
 collection = db['user_infor']
