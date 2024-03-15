8c09819f975a3b19a71bd50fd4183450ddc99e52
kchen256 <m1371339094@gmail.com>
2024-03-15 17:07:32 -0400

    import_file

--- Server.py
+++ Server.py
@@ -1,32 +1,32 @@
-from flask import Flask,make_response,request
+from flask import Flask,make_response,request,redirect, url_for
 from pymongo import MongoClient
-
+from json import dumps,loads
 app = Flask(__name__)
-try:
-    mongo_client = MongoClient("localhost")
-    db = mongo_client['user_atabase']
-    collection = db['user_infor']
-except Exception as erro:
-    print("error in connecting to the Database")
+
+mongo_client = MongoClient("localhost")
+db = mongo_client['user_database']
+collection = db['user_infor']
+
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    if request.method == 'GET':
+    # if request.method == 'GET':
+        print("here")
         with open("./public/index.html","r") as file:
             file = file.read()
             response = make_response(file)
             response.headers['Content-Type'] = 'text/html; charset=utf-8'
             response.headers['X-Content-Type-Options'] = 'nosniff'
             return response
-    else:
-        data = request.json
-        print(data)
-    with open("./public/index.html","r") as file:
-            file = file.read()
-            response = make_response(file)
-            response.headers['Content-Type'] = 'text/html; charset=utf-8'
-            response.headers['X-Content-Type-Options'] = 'nosniff'
-            return response
+    # else:
+    #     data = request.json
+    #     print(data["user"])
+    # with open("./public/index.html","r") as file:
+    #         file = file.read()
+    #         response = make_response(file)
+    #         response.headers['Content-Type'] = 'text/html; charset=utf-8'
+    #         response.headers['X-Content-Type-Options'] = 'nosniff'
+    #         return response
 @app.route("/functions.js")
 def func():
     with open("./public/function.js","r") as file:
@@ -43,13 +43,22 @@ def style():
         response.headers['Content-Type'] = 'text/css; charset=utf-8'
         response.headers['X-Content-Type-Options'] = 'nosniff'
         return response
-@app.route("/register", methods=['POST'])
+@app.route("/favicon.ico")
+def fav():
+    with open("./public/style.css","r") as file:
+        file = file.read()
+        response = make_response(file)
+        response.headers['Content-Type'] = 'text/css; charset=utf-8'
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+@app.route("/register", methods=['GET','POST'])
 def get_data():
-    # print(msg)
+    # print(request)
     data = request.json
-    print(data)
+    data.get("reg_user")
+    data.get("reg_pass")
+    return redirect("/favicon.ico",302)
     
-    return "nothing"  
 if __name__ == '__main__':
     app.run(host='localhost', port=8080,debug=True)#debug=True
 
--- public/function.js
+++ public/function.js
@@ -2,11 +2,17 @@ function send_reg() {
     const user = document.getElementById("reg_user").value;
     const pass = document.getElementById("reg_pass").value;
     const data = { user: user, pass: pass };
-    fetch('/', {
+    
+    fetch('/register', {
         method: 'POST',
+        redirect: "follow",
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true 
         },
         body: JSON.stringify(data)
-    }).then(response => {response.json();})
+    })
+    .then(response => response.json())
+    ;
 }
