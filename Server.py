d94fec7591ec65bf0412af00985f25fd692b7cfb
zhuohany <zhuohanye@gmail.com>
2024-03-19 15:52:44 -0400

    Finished the login. Check the login, if login is correct issue auth_token. Auth_token stored into data base. Auth_token added to cookie. If successful return 302, else return 404.

--- Server.py
+++ Server.py
@@ -3,12 +3,13 @@ from pymongo import MongoClient
 from json import dumps,loads
 import bcrypt
 import uuid
+import hashlib
 app = Flask(__name__)
 
 mongo_client = MongoClient("mongo")
 db = mongo_client['user_database']
 collection = db['user_infor']
-
+auth_collection = db['auth_db']
 
 @app.route("/", methods=['GET','POST'])
 def index():
@@ -17,6 +18,7 @@ def index():
         with open("./public/index.html","r") as file:
             file = file.read()
             response = make_response(file)
+            response.headers["Set-Cookie"] = 'Cookie=1' 
             response.headers['Content-Type'] = 'text/html; charset=utf-8'
             response.headers['X-Content-Type-Options'] = 'nosniff'
             return response
@@ -58,6 +60,21 @@ def get_data():
 
     return redirect("/",302)
     
+@app.route("/login", methods=['GET','POST'])
+def login():
+    # print(request)
+    data = request.form
+    thisitem = collection.find_one({"username":data.get("login_user")})
+    if bcrypt.checkpw(data.get("login_passs").encode(),thisitem["password"]) == True:
+        auth_token = uuid.uuid4()
+        hashtoken = hashlib.sha256(str(auth_token).encode()).hexdigest()
+        #at = 'auth_token='+str(auth_token)
+        response = make_response(redirect('/',302))
+        response.set_cookie('auth_token', str(auth_token))
+        auth_collection.insert_one({"username":data.get("login_user"),"auth_toke":hashtoken})
+        return response
+    return abort(404)
+
 if __name__ == '__main__':
     #port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=8080,debug=True)#debug=True
--- public/function.js
+++ public/function.js
@@ -17,3 +17,23 @@ function send_reg() {
     ;
 }
 
+
+function login() {
+    const user = document.getElementById("login_user").value;
+    const pass = document.getElementById("login_passs").value;
+    const data = { user: user, pass: pass };
+    
+    fetch('/register', {
+        method: 'POST',
+        redirect: "follow",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true 
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    ;
+}
+
